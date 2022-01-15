f5eb6cdcee5887c113880374efab1dae53f5d4d9
Sam <beardmans72@gmail.com>
2022-01-16 10:18:49 +1300

    Set folder back to original

--- terragrunt/Deploy-GCPTerragruntConfig.ps1
+++ terragrunt/Deploy-GCPTerragruntConfig.ps1
@@ -15,45 +15,61 @@ param (
 	[Bool]$PlanOnly = $false
 )
 
-# Set env var to tell Terraform it's running in automation pipeline
-$env:TF_IN_AUTOMATION = "true"
-
-# Create a low-level folder for the TG cache so that we don"t run in to errors with long files names
-$TGCachePath = "c:\tgcache"
-$null = New-Item `
-	-Path $TGCachePath `
-	-ItemType Directory `
-	-Force
-
-$env:TERRAGRUNT_DOWNLOAD = $TGCachePath
-
-# Build path for terragrunt config file
-$TerragruntPath = Join-Path `
-	-Path $PSScriptRoot `
-	-ChildPath "gcp" `
-	-AdditionalChildPath @($Project, $Service)
-
-Write-Host "Setting location to $TerragruntPath"
-$null = Set-Location -Path $TerragruntPath
-
-Write-Host "Running terragrunt init"
-& terragrunt init --terragrunt-source-update
-
-Write-Host "Running terragrunt plan"
-& terragrunt plan -out=tfplan -input=false
-
-if ($LASTEXITCODE -ne 0)
+try
 {
-	throw "Terragrunt plan failed."
-}
+	# Set env var to tell Terraform it's running in automation pipeline
+	$env:TF_IN_AUTOMATION = "true"
 
-if (-not $PlanOnly)
-{
-	Write-Host "Running terragrunt apply"
+	# Create a low-level folder for the TG cache so that we don"t run in to errors with long files names
+	$TGCachePath = "c:\tgcache"
+	$null = New-Item `
+		-Path $TGCachePath `
+		-ItemType Directory `
+		-Force
 
-	& terragrunt apply -auto-approve -input=false tfplan
+	$env:TERRAGRUNT_DOWNLOAD = $TGCachePath
+
+	$CurrentPath = $PSScriptRoot
+
+	# Build path for terragrunt config file
+	$TerragruntPath = Join-Path `
+		-Path $PSScriptRoot `
+		-ChildPath "gcp" `
+		-AdditionalChildPath @($Project, $Service)
+
+	Write-Host "Setting location to $TerragruntPath"
+	$null = Set-Location -Path $TerragruntPath
+
+	Write-Host "Running terragrunt init"
+	& terragrunt init --terragrunt-source-update
+
+	Write-Host "Running terragrunt plan"
+	& terragrunt plan -out=tfplan -input=false
+
 	if ($LASTEXITCODE -ne 0)
 	{
-		throw "Terragrunt apply failed."
+		throw "Terragrunt plan failed."
+	}
+
+	if (-not $PlanOnly)
+	{
+		Write-Host "Running terragrunt apply"
+
+		& terragrunt apply -auto-approve -input=false tfplan
+		if ($LASTEXITCODE -ne 0)
+		{
+			throw "Terragrunt apply failed."
+		}
+	}
+}
+catch
+{
+	throw $_
+}
+finally
+{
+	if ($CurrentPath)
+	{
+		Set-Location -Path $CurrentPath | Out-Null
 	}
 }
