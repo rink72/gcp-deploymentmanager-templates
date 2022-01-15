082b8843604ce1b49714ed31fb2f450abdc0fb76
Sam <beardmans72@gmail.com>
2022-01-15 22:40:00 +1300

    Add init script for gcp state bucket

---
+++ init/gcp-init.ps1
@@ -0,0 +1,28 @@
+#Requires -Modules GoogleCloud
+
+[CmdletBinding()]
+param (
+	[Parameter()]
+	[String]$BucketName = "rink72-lab-infra"
+)
+
+$ErrorActionPreference = "Stop"
+
+Import-Module `
+	-Name GoogleCloud `
+	-Force
+
+$ExistingBucket = Get-GcsBucket | Where-Object { $_.Name -eq $BucketName }
+
+if ($ExistingBucket)
+{
+	Write-Host "State bucket <$BucketName> already exists"
+}
+else
+{
+	New-GcsBucket `
+		-Name $BucketName | Out-Null
+
+	Write-Host "State bucket <$BucketName> created"
+}
+
